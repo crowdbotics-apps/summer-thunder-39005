46eb37af23ff625385337d4b98313f7f10989ddb
Crowdbotics <team@crowdbotics.com>
2023-02-18 09:04:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-thunder-39005",
+  url: "https://summer-thunder-39005.botics.co",
+  api: "https://summer-thunder-39005.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
